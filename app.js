0b8608d0e1227cddc000a7d664f70111408c954d
gjabble <gurpsjabble@gmail.com>
2020-02-13 12:57:41 +0000

    initial session setup

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var uuid = require('uuid');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// session set up
+app.use(session({
+  genid: function (req) {
+    return uuid() // use UUIDs for session IDs
+  },
+  secret: 'some-secret',
+  cookie: {
+    maxAge: 60000
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,13 +41,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// handler for login request
+// at the moment it does nothing
+app.post('/login', (req, res) => {
+  console.log(req.session);
+  res.send();
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
