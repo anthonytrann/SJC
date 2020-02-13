79da124f89d52baaa89d0f36df59074af8f20a6c
anthonytran227 <33399044+anthonytran227@users.noreply.github.com>
2020-02-13 18:03:39 +0700

    Uploading file

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+
+router.get('/homepage',function(req, res) {
+  res.render('homepage');
+});
+
+
+module.exports = router;
